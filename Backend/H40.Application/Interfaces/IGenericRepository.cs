b41d798677348483359f916315d98dd8b806d28c
Ahmed <ahmed.my@h40.com>
2026-09-07 16:50:00 +0300

    feat(api): Create Generic Repository pattern for data access

---
+++ Backend/H40.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace H40.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
