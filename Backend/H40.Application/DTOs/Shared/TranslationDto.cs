8cceebca87259e8a9f629cd08fcb5151b40d2d24
Ahmed <ahmed.my@h40.com>
2026-09-13 15:45:00 +0300

    feat(api): Create Translation specific DTOs and mappings

---
+++ Backend/H40.Application/DTOs/Shared/TranslationDto.cs
@@ -0,0 +1,8 @@
+namespace H40.Application.DTOs.Shared
+{
+    public class TranslationDto
+    {
+        public string LanguageCode { get; set; } = string.Empty;
+        public string TranslatedText { get; set; } = string.Empty;
+    }
+}
